bad5e55abf87514b6bc60c8c479565e6c8b46679
MiriamLaw <mimsical999@yahoo.com>
2024-02-13 20:12:27 -0800

    Updated to Java 17
    Unit tests are working now phew

--- src/main/java/com/coderscampus/assignment13/web/UserController.java
+++ src/main/java/com/coderscampus/assignment13/web/UserController.java
@@ -4,13 +4,12 @@ import java.util.Arrays;
 import java.util.Optional;
 import java.util.Set;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -104,7 +103,7 @@ public class UserController {
 	}
 
 	@PostMapping("/users/{userId}/accounts")
-	public String createAccountForUser(@PathVariable Long userId, @Valid @ModelAttribute("account") Account account,
+	public String createAccountForUser(@PathVariable Long userId, @Validated @ModelAttribute("account") Account account,
 			BindingResult result) {
 		if (result.hasErrors()) {
 			return "account";
